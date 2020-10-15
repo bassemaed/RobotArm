a099d6e94b0699e78f5e0083685581810dd975f1
bassemaed <bassem.alaa.edine@gmail.com>
2020-10-15 13:59:19 +0200

    Initial version of inverse kinematics code

--- StanfordManipulator_RTB/StanfordMan2.m
+++ StanfordManipulator_RTB/StanfordMan2.m
@@ -15,7 +15,7 @@ d0      =   [L1.d       L2.d            10          L4.d        L5.d        L6.d
 MyRobotArm.qlim = qlim;
 T = [cos(theta0(1))  -sin(theta0(1))*cos(alpha0(1))  sin(theta0(1))*sin(alpha0(1))   0;
      sin(theta0(1))  cos(theta0(1))*cos(alpha0(1))   -cos(theta0(1))*sin(alpha0(1))  0;
-     0               sin(alpha0(1))                  cos(alpha0(1))                  d0(1); 
+     0               sin(alpha0(1))                  cos(alpha0(1))                  d0(1);
      0               0                               0                               1];
 for i=2:6
     temp = [cos(theta0(i))  -sin(theta0(i))*cos(alpha0(i))  sin(theta0(i))*sin(alpha0(i))   0;
@@ -24,4 +24,12 @@ for i=2:6
             0               0                               0                               1];
     T = T*temp;
 end
-MyRobotArm.plot(q0);
+%MyRobotArm.plot(q0);
+InvKin = zeros(6,2);
+T2 = [-0.6597   0.7367  0.1484  5;
+      0.4356    0.2140  0.8743  8.6603;
+      0.6124    0.6415  -0.4621 5;
+      0         0       0       1]
+r =  sqrt((T2(1,4))^2 + (T2(2,4))^2);
+InvKin(1,1) = atan2(T2(2,4),T2(1,4)) - atan2(L2.d,sqrt(r^2-(L2.d)^2));
+InvKin(1,2) = atan2(T2(2,4),T2(1,4)) - atan2(L2.d,-sqrt(r^2-(L2.d)^2));
