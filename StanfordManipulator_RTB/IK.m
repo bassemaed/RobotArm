3955a78a7e51cfce3ce6e04f7005daaffe3db772
bassemaed <bassem.alaa.edine@gmail.com>
2020-10-15 17:40:16 +0100

    Added a function for inverse kinematics computation

---
+++ StanfordManipulator_RTB/IK.m
@@ -0,0 +1,48 @@
+function [OutResult] = IK(InTMat,InLim, Ind2)
+%T =    u_x ..  v_x ..  w_x ..  p_x
+%       u_y ..  v_y ..  w_y ..  p_y
+%       u_z ..  v_z ..  w_z ..  p_z
+%       0   ..  0   ..  0   ..  1
+OutResult = zeros(6,1);
+u_x = InTMat(1,1);
+u_y = InTMat(2,1);
+u_z = InTMat(3,1);
+v_x = InTMat(1,2);
+v_y = InTMat(2,2);
+v_z = InTMat(3,2);
+w_x = InTMat(1,3);
+w_y = InTMat(2,3);
+w_z = InTMat(3,3);
+p_x = InTMat(1,4);
+p_y = InTMat(2,4);
+p_z = InTMat(3,4);
+r =  sqrt(p_x^2 + p_y^2);
+OutResult(1) = atan2(p_y,p_x) - atan2(Ind2,sqrt(r^2-(Ind2)^2));
+if(OutResult(1) >InLim(1,2) || OutResult(1)<InLim(1,1))
+    OutResult(1) = atan2(p_y,p_x) - atan2(Ind2,-sqrt(r^2-(Ind2)^2));
+end
+c_1 = cos(OutResult(1));
+s_1 = sin(OutResult(1));
+OutResult(2) = atan2(c_1*p_x + s_1*p_y, p_z);
+s_2 = sin(OutResult(2));
+c_2 = cos(OutResult(2));
+OutResult(3) = (p_x *c_1 + s_1*p_y)*(s_2) + p_z*c_2;
+term_1 =  -s_1*w_x + c_1*w_y;
+term_2 = c_2*(c_1*w_x + s_1*w_y) - s_2*w_z;
+OutResult(4) = atan2(term_1,term_2);
+c_4 = cos(OutResult(4));
+s_4 = sin(OutResult(4));
+term_1 = c_4*(c_2*(c_1*w_x+s_1*w_y)-s_2*w_z)+ ...
+         s_4*(c_1*w_y -s_1*w_x);
+term_2 = s_2*(c_1*w_x +s_1*w_y) +c_2*w_z;
+OutResult(5) = atan2(term_1,term_2);
+I = c_1*v_x + s_1*v_y;
+nterm = -s_1*v_x + c_1*v_y;
+s_5 = sin(OutResult(5));
+c_5 = cos(OutResult(5));
+term_1 = -c_5*(c_4*(c_2*I-s_2*v_z)+s_4*nterm)+ ...
+         s_5*(s_2*I +c_2*v_z);
+term_2 = -s_4*(c_2*I -s_2*v_z)+c_4*nterm;
+OutResult(6) = atan2(term_1,term_2);
+end
+
--- StanfordManipulator_RTB/StanfordMan2.m
+++ StanfordManipulator_RTB/StanfordMan2.m
@@ -26,16 +26,17 @@ theta0  =   [pi/6   pi/3    0   pi/4    5*pi/12 pi/2];
 d0      =   [L1.d   L2.d    q0(3)  L4.d    L5.d    L6.d];
 %Position 0 - Joint twist angles
 alpha0  =   [L1.alpha   L2.alpha    L3.alpha    L4.alpha    L5.alpha    L6.alpha];
-%Calculating Transformation Matrix T01_0
+%Calculating Transformation Matrix T06_0
 T06_0 = FK(6,theta0,d0,alpha0);
 %Ploting the RobotArm
 %MyRobotArm.plot(q0);
 %Inverse Kinematics Solution
 InvKin = zeros(6,1);
-T2 = [-0.6597   0.7367  0.1484  5;
+%Example Transformation Matrix T06_Input
+%Input Variables [pi/6, pi/3, 10, pi/4, 5*pi/12, pi/2]
+T06_Input = [-0.6597   0.7367  0.1484  5;
       0.4356    0.2140  0.8743  8.6603;
       0.6124    0.6415  -0.4621 5;
       0         0       0       1];
-r =  sqrt((T2(1,4))^2 + (T2(2,4))^2);
-InvKin(1,1) = atan2(T2(2,4),T2(1,4)) - atan2(L2.d,sqrt(r^2-(L2.d)^2));
-InvKin(1,2) = atan2(T2(2,4),T2(1,4)) - atan2(L2.d,-sqrt(r^2-(L2.d)^2));
+InvKin = IK(T06_Input,qlim,L2.d);
+         
